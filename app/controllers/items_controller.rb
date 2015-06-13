2e92a9f12c5e2e5efe30ca73aea9f38a61db029a
MR.Y <programming@yatsukouji.com>
2015-06-13 16:42:57 +0900

    indexページを作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+
   def show
     @item = Item.find(params[:id])  #めちゃくちゃ大事。インスタンス変数のitemとparamsメソッド
   end
