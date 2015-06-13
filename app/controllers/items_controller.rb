47d9ef9274f38b88c3abfc1349be021c98a2b295
MR.Y <programming@yatsukouji.com>
2015-06-13 18:07:51 +0900

    新規機能実装。登録に関して。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,39 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
-
   def show
-    @item = Item.find(params[:id])  #めちゃくちゃ大事。インスタンス変数のitemとparamsメソッド
+    @item = Item.find(params[:id])
+    #めちゃくちゃ大事。インスタンス変数のitemとparamsメソッド
   end
+
+  def new
+    @item = Item.new
+  end
+
+
+  def create
+    #formからデータを受け取る
+    @item = Item.new(item_params)
+    #これを保存する
+    @item.save
+    #show.html.erbに飛ばす
+    redirect_to "/items/#{@item.id}"
+  end
+
+ private
+
+ def item_params
+# params.require(:key).permit(:filter)
+   params.require(:item).permit(
+    :name,
+    :price,
+    :seller_id,
+    :description,
+    :email,
+    :image_url
+  )
+ end
+
+
+
 end
